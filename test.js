e16abd0097121162f8b80a6abf318bcbe71ffb54
Matt <mjezorek@gmail.com>
2017-09-09 08:55:14 -0700

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
- var cryptopia = require('./index.js');
- var c = new cryptopia('997f96b08ced4c96880849ca480edca8','JPaabD55jAhB5Z8fhNuAVCtPNihG5Ng8GDcBIVEX88U=');
-
-  c.getmarkethistory('PASL_BTC', 24, function(err, data) {
-  	console.log(err);
-  	console.log(data);
-  });
